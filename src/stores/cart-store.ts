84936882fea6320f903d5fac96f7adbd7bc1431c
fespschulte <fschultepinto@gmail.com>
2024-02-13 11:35:39 -0300

    :sparkles: Add Remove and Add methods

---
+++ src/stores/cart-store.ts
@@ -0,0 +1,25 @@
+import { ProductProps } from "@/utils/data/products";
+import { create } from "zustand";
+import * as cartInMemory from "./helpers/cart-in-memory";
+
+export interface ProductCartProps extends ProductProps {
+  quantity: number;
+}
+
+export interface StateProps {
+  products: ProductCartProps[];
+  add: (product: ProductProps) => void;
+  remove: (productId: string) => void;
+}
+
+export const useCartStore = create<StateProps>((set) => ({
+  products: [],
+  add: (product: ProductProps) =>
+    set((state) => ({
+      products: cartInMemory.add(state.products, product),
+    })),
+  remove: (productId: string) =>
+    set((state) => ({
+      products: cartInMemory.remove(state.products, productId),
+    })),
+}));
